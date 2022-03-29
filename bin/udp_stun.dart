db59c6cde3bee8c6ef856eb12c28ec33de756bc9
Oussama Maatallah <oussamamaatallah01@gmail.com>
2022-03-29 16:43:34 +0100

    added a third parameter

--- bin/udp_stun.dart
+++ bin/udp_stun.dart
@@ -4,9 +4,10 @@ import 'package:udp/udp.dart';
 void main(List<String> args) async {
   var p1 = int.parse(args[0]);
   var p2 = int.parse(args[1]);
+  var ip2 = args[2];
   print("Starting Awesome Chatapp...");
   var endp1 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: Port(p1));
-  var endp2 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: Port(p2));
+  var endp2 = Endpoint.unicast(InternetAddress(ip2), port: Port(p2));
   var socket = await UDP.bind(endp1);
 
   stdout.write("PEER1: ");
