5b8c56a1ee8bddde84ac3edd7faedd248316ac32
Oussama Maatallah <oussamamaatallah01@gmail.com>
2022-03-29 16:30:34 +0100

    nothing new, really

---
+++ bin/udp_stun.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:udp/udp.dart';
+
+void main(List<String> args) async {
+  var p1 = int.parse(args[0]);
+  var p2 = int.parse(args[1]);
+  print("Starting Awesome Chatapp...");
+  var endp1 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: Port(p1));
+  var endp2 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: Port(p2));
+  var socket = await UDP.bind(endp1);
+
+  stdout.write("PEER1: ");
+  socket.asStream().listen((event) {
+    print("PEER2: " + String.fromCharCodes(event!.data));
+    stdout.write("PEER1: ");
+  });
+
+  stdin.listen((event) {
+    socket.send(event, endp2);
+    stdout.write("PEER1: ");
+  });
+}
