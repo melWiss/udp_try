66fe62090a1c0dd9c3aa0f183df5162d71510586
Oussama Maatallah <oussamamaatallah01@gmail.com>
2022-04-16 17:39:20 +0100

    created a new peer client

--- bin/udp_stun_2.dart
+++ bin/udp_stun_2.dart
@@ -1,23 +1,73 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'package:udp/udp.dart';
 
 void main(List<String> args) async {
-  var p1 = int.parse(args[0]);
-  var p2 = int.parse(args[1]);
-  var ip2 = args[2];
+  Map<String, int> peers = {};
+  var p1 = Port(55002);
   print("Starting Awesome Chatapp...");
-  var endp1 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: Port(p1));
-  var endp2 = Endpoint.unicast(InternetAddress(ip2), port: Port(p2));
+  var endp1 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: p1);
   var socket = await UDP.bind(endp1);
 
-  stdout.write("PEER1: ");
+  // connect to RECONN SERVER
+  socket.send(
+    [0],
+    Endpoint.unicast(
+      InternetAddress(args.isNotEmpty ? args[0] : "102.158.140.104"),
+      port: Port(args.length > 1 ? int.parse(args[1]) : 55001),
+    ),
+  );
+  Timer.periodic(Duration(seconds: 20), (t) {
+    socket.send(
+      [0],
+      Endpoint.unicast(
+        InternetAddress(args.isNotEmpty ? args[0] : "102.158.140.104"),
+        port: Port(args.length > 1 ? int.parse(args[1]) : 55001),
+      ),
+    );
+  });
+
+  // listen for connections
   socket.asStream().listen((event) {
-    print("PEER2: " + String.fromCharCodes(event!.data));
-    stdout.write("PEER1: ");
+    String response = String.fromCharCodes(event!.data);
+    try {
+      Map<String, dynamic> responseData = jsonDecode(response);
+      if (responseData["type"] == "CONNECTIONS") {
+        peers = responseData["data"];
+        connectAllPeers(peers, socket);
+      } else if (responseData["type"] == "DATA") {
+        print("${event.address.address}:${event.port}/ $responseData");
+      }
+      // ignore: empty_catches
+    } catch (e) {}
   });
 
   stdin.listen((event) {
-    socket.send(event, endp2);
-    stdout.write("PEER1: ");
+    sendToAllPeers(peers, socket, event);
+  });
+}
+
+connectAllPeers(Map<String, int> peers, UDP socket) {
+  peers.forEach((ip, port) {
+    socket.send(
+      [0],
+      Endpoint.unicast(
+        InternetAddress(ip),
+        port: Port(port),
+      ),
+    );
+  });
+}
+
+sendToAllPeers(Map<String, int> peers, UDP socket, List<int> data) {
+  peers.forEach((ip, port) {
+    socket.send(
+      data,
+      Endpoint.unicast(
+        InternetAddress(ip),
+        port: Port(port),
+      ),
+    );
   });
 }
