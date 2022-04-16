604e14b75e88224fb49bdaec89e05cb80eb9ef7f
Oussama Maatallah <oussamamaatallah01@gmail.com>
2022-04-16 16:05:00 +0100

    added ip sharer server

---
+++ bin/udp_peers_ip_server.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:udp/udp.dart';
+
+void main(List<String> args) async {
+  var p1 = args.isNotEmpty ? Port(int.parse(args[0])) : Port(55001);
+  var endp1 = Endpoint.unicast(InternetAddress("0.0.0.0"), port: p1);
+  var socket = await UDP.bind(endp1);
+  print(
+      "Starting RECON-SERVER://${endp1.address!.address}:${endp1.port!.value}");
+
+  Map<String, int> ips = {};
+
+  socket.asStream().listen(
+    (event) async {
+      print("REQUEST ${event!.address.address}:${event.port} ");
+      if (ips.containsKey(event.address.address)) {
+        if (ips[event.address.address] != event.port) {
+          ips[event.address.address] = event.port;
+          //notify all peers
+          notifyAllPeers(ips, socket);
+          print(ips);
+        }
+      } else {
+        ips.addAll({event.address.address: event.port});
+        //notify all peers
+        notifyAllPeers(ips, socket);
+        print(ips);
+      }
+      // notifyAllPeers(ips, socket);
+    },
+    onDone: () => print("done"),
+    onError: (e) => print("error"),
+  );
+}
+
+notifyAllPeers(Map<String, int> ips, UDP socket) {
+  ips.forEach((key, value) async {
+    socket.send(
+      jsonEncode(ips).codeUnits,
+      Endpoint.unicast(
+        InternetAddress(key),
+        port: Port(value),
+      ),
+    );
+  });
+}
